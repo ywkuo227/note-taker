5dca7102501818479d486f505b57d7f154c47415
ywkuo227 <ywkuo@outlook.com>
2021-07-25 17:52:45 -0700

    July 25th code update #4: - Initial Heroku deployment. - Modified Port configuration and package.json.

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const api = require("./public/assets/js/api");
 
 // Set up the port the Express server will be listening to.
 const app = express();
-var PORT = process.env.PORT || 3001;
+var PORT = process.env.PORT || 3000;
 
 // Middleware for parsing JSON and urlencoded form data.
 app.use(express.json());
